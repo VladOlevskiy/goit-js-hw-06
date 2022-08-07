ac9c107e185dde6a0a1bea2d44d4bb9fc45b5e63
VladOlevskiy <99341001+VladOlevskiy@users.noreply.github.com>
2022-08-07 11:13:19 +0300

    remark

--- js/task-01.js
+++ js/task-01.js
@@ -4,10 +4,10 @@ console.log(listItemsAmount);
 
 const liEl = document.querySelectorAll('li.item');
 const arrayOfLi = Array.from(liEl);
-const aa = array => {
+const getTitleAndAmount = array => {
   array.map((element, index) => {
     console.log(`Category: ${array[index].firstElementChild.textContent}`);
     console.log(`Elements: ${array[index].lastElementChild.children.length}`);
   });
 };
-aa(arrayOfLi);
+getTitleAndAmount(arrayOfLi);
